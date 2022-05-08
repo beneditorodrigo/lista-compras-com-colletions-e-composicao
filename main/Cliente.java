7ccdfb7f95fde781f0597f070cb8bae7aa3edfb3
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-05-08 08:57:34 -0300

    Lista de compras para Cliente

--- main/Cliente.java
+++ main/Cliente.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
 
+	//cada cliente possui um nome e uma lista de compras
+	List<Compra> compras = new ArrayList<>();
 }

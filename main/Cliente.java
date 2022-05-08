b528b8d8257ac58d89f42d39cdd0a60c617bfc27
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-05-08 09:28:39 -0300

    Métodos para cliente realizar uma compra e saber valor gasto por um cliente

--- main/Cliente.java
+++ main/Cliente.java
@@ -5,6 +5,28 @@ import java.util.List;
 
 public class Cliente {
 
-	//cada cliente possui um nome e uma lista de compras
+	// cada cliente possui um nome e uma lista de compras
+	String nome;
 	List<Compra> compras = new ArrayList<>();
+
+	// Construtor da classe cliente
+	Cliente(String nome) {
+		this.nome = nome;
+	}
+
+	// cliente fazendo uma compra
+	void realizarUmaCompra(Compra compra) {
+		compras.add(compra);
+	}
+
+	// total gasto por um cliente em todas as compras
+	double totalDeComprasPorCliente() {
+		double totalGastoPorCliente = 0;
+
+		for (Compra compra : compras) {
+			totalGastoPorCliente += compra.valorDaCompra();
+		}
+
+		return totalGastoPorCliente;
+	}
 }

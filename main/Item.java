8bcd0401bee0e4063bcf55c217c7c9bad2e75977
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-05-08 08:49:31 -0300

    Criando a classe Item com um Produto associado

--- main/Item.java
+++ main/Item.java
@@ -2,4 +2,14 @@ package main;
 
 public class Item {
 	
+	//Item possui um produto associado e a quantidade
+	int quantidade;
+	Produto produto;
+	
+	//construtor da classe Item
+	Item(Produto produto, int quantidade){
+		this.produto = produto;
+		this.quantidade = quantidade;
+	}
+	
 }

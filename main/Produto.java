1bd55578f9f775855f4fc24f9d66590ed15439d0
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-05-08 08:44:00 -0300

    Criando a classe Produto e toda a estrutura de classes do programa

---
+++ main/Produto.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Produto {
+
+	//produto possui nome e preço
+	String nome;
+	double preco;
+	
+	//construtor da classe Produto
+	Produto (String nome, double preco){
+		this.nome = nome;
+		this.preco = preco;
+	}
+}
---
+++ main/Sistema.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Sistema {
+
+	//classe e método para testar o sistema
+	public static void main(String[] args) {
+		
+	}
+}

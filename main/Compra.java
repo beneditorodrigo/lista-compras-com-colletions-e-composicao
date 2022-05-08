2a866662aae890a41ef2978afde3f26525db8e74
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-05-08 09:06:38 -0300

    Criando os métodos para adcionar um item a lista de compra

--- main/Compra.java
+++ main/Compra.java
@@ -5,7 +5,19 @@ import java.util.List;
 
 public class Compra {
 
+	//lista de itens para a compra
 	List<Item> itens = new ArrayList<>();
 	
+	//vou criar dois metodos para adcionar itens na compra
+	//um metodo com o Produto já instanciado anteriormente
+	void adcionarItemCompra(Produto produto, int quantidade) {
+		itens.add(new Item(produto, quantidade));
+	}
 	
+	//outro método que instancia um novo Produto dentro do próprio método	
+	void adcionarItemCompra(String nome, double preco, int quantidade) {
+		//inferencia
+		var produto = new Produto(nome, preco);
+		itens.add(new Item(produto, quantidade));
+	}
 }
